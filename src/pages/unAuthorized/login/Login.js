c9228043223589bf73f1defa5ec49b605f61ae15
bakhtawarneaz <bakhtawarneaz@gmail.com>
2024-06-21 01:58:49 +0500

    email to username

--- src/pages/unAuthorized/login/Login.js
+++ src/pages/unAuthorized/login/Login.js
@@ -3,7 +3,7 @@ import { useMutation } from '@tanstack/react-query';
 import { useDispatch } from 'react-redux';
 import { login as loginAction } from '../../../features/auth/authSlice';
 import { login } from '../../../services/authApi';
-//import { useHistory } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { useSkin } from "@hooks/useSkin";
 import illustrationsLight from "@src/assets/images/pages/login-v2.svg";
 import illustrationsDark from "@src/assets/images/pages/login-v2-dark.svg";
@@ -28,17 +28,17 @@ const Login = () => {
   const { skin } = useSkin();
   const source = skin === "dark" ? illustrationsDark : illustrationsLight;
 
-  const [email, setEmail] = useState('');
+  const [username, setUserName] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
   const dispatch = useDispatch();
- // const history = useHistory();
+  const navigate = useNavigate();
 
   const mutation = useMutation({
     mutationFn: login,
     onSuccess: (data) => {
       dispatch(loginAction(data));
-      //history.push('/dashboard');
+      navigate('/home');
     },
     onError: () => {
       //setError('Invalid credentials');
@@ -48,12 +48,12 @@ const Login = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (!email || !password) {
+    if (!username || !password) {
       //setError('Email and password are required');
-      toast.error('Email and password are required')
+      toast.error('Username and password are required')
       return;
     }
-    mutation.mutate({ email, password });
+    mutation.mutate({ username, password });
   };
 
 
@@ -82,15 +82,15 @@ const Login = () => {
               onSubmit={handleSubmit}
             >
               <div className="mb-1">
-                <Label className="form-label" for="login-email">
-                  Email
+                <Label className="form-label" for="login-username">
+                 User Name
                 </Label>
                 <Input
-                  type="email"
-                  id="email"
-                  value={email}
-                  placeholder="john@example.com"
-                  onChange={(e) => setEmail(e.target.value)}
+                  type="text"
+                  id="username"
+                  value={username}
+                  placeholder="admin"
+                  onChange={(e) => setUserName(e.target.value)}
                   autoFocus
                 />
               </div>
